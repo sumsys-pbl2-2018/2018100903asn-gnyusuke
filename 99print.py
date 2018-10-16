647d9cbff190b01ed1796005eb4cb829a430e02f
gnyusuke <32833390+gnyusuke@users.noreply.github.com>
2018-10-16 15:55:59 +0900

    Add files via upload

---
+++ 99print.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# This is a program calculating the total amount of money
+
+for i in range(1,10):
+    for j in range(1,10):
+        sum = i*j
+        print("%4d"%sum,end="")
+    print() 
